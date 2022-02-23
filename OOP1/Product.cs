339392a6e94f9fbe12b979a6a1ea5b1abe940b63
burcutas636@gmail.com <burcutas636@gmail.com>
2022-02-23 23:22:27 +0300

    Add project files.

---
+++ OOP1/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    //snippet: "Hazır kodlar"a snippet denir.
+    internal class Product
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }  //kategori id'isi
+        public string ProductName { get; set; }  //ürün ismi
+        public double UnitPrice { get; set; }  //ürünün birim fiyatı
+        public int UnitInStock { get; set; }  //ürünün stok adeti
+    }
+}
